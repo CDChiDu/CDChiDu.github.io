2bd08ad1e6accfc4b55fc789a470dfeff3e7753a
ChiDuCD <hzh15726596703@gmail.com>
2026-06-16 13:00:24 +0800

    Site updated: 2026-06-16 13:00:23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1781586005212";const preCache = ["/images/taichi.png","/images/wallpaper.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1781586014277";const preCache = ["/images/taichi.png","/images/wallpaper.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
